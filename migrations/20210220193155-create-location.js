88698ec1a9382f5ad33b29b3ff536d9cde26e50a
CHRISDAVIDSPICER <CHRISDAVIDSPICER@GMAIL.COM>
2021-02-20 11:38:26 -0800

    Setup initial API

---
+++ migrations/20210220193155-create-location.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('locations', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      street: {
+        type: Sequelize.STRING
+      },
+      region: {
+        type: Sequelize.STRING
+      },
+      bus: {
+        type: Sequelize.INTEGER
+      },
+      restaurantId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('locations');
+  }
+};
